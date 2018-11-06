599d078a0767dea07f0ed98428a09847ae53b76e
CassianoSF <cassianosfranco@gmail.com>
2018-11-06 00:12:50 -0200

    coment e refactor game.cpp 1

--- game.cpp
+++ game.cpp
@@ -1,8 +1,10 @@
-int* keyStates = new int[256];
-int screen_w = 1000;
-int screen_h = 700;
-int game_time = 0;
-int frame_time = 100;
+int* keyStates = new int[256]; // Estado das teclas 0 e 1
+int screen_w = 1000;           // largura da tela
+int screen_h = 700;            // altura da tela
+int game_clock = 0;            // incremento do updateGame
+int frame_time = 100;          // divisor do clock (frame window)
+int time_flag = 0;             // flag de atualização
+
 
 //       Color(r, g, b, a)
 Color    CINZA(0.5, 0.5, 0.5, 1.0);
@@ -40,11 +42,10 @@ Player player(knife, 0.0f, 0.0f, 10.0f, 0, PRETO);
 
 
 
-void myDisplay(void){
+void renderGame(void){
     // REDERIZAÇÃO
     glLoadIdentity();
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    // glTranslatef(-player.pos.x, -player.pos.y, -35);
     glTranslatef(-player.pos.x, -player.pos.y, -35);
     glRotatef(0, 1, 1, 1);
 
@@ -53,77 +54,25 @@ void myDisplay(void){
     obstaculo_0.render();
     obstaculo_1.render();
     obstaculo_2.render();
-    player.render(textures, game_time, frame_time);
+    player.render(textures, game_clock, frame_time);
 
     glFlush();  // Requisita que o buffer usado para as operações de renderização seja exibido na tela
     glutSwapBuffers();
     glutPostRedisplay();
 }
 
-void loadTextures(){
-    glClearColor(0,0,0,1);
-    // glDepthMask(GL_FALSE);
-
-    std::string path = "./_player/";
-    string path_string_array[(421+43)];
-    string str_filename; 
-    int i = 0;
-    for (auto & p : fs::directory_iterator(path)){
-        for (auto & pp : fs::directory_iterator(p)){
-            for (auto & ppp : fs::directory_iterator(pp)){
-                str_filename = ppp.path().string().c_str();
-                path_string_array[i] = ppp.path().string().c_str();
-                char *filename = new char[path_string_array[i].size()+1];
-                strncpy(filename, path_string_array[i].c_str(), path_string_array[i].size());
-                textures[i].data = png_texture_load(filename, NULL, NULL);
-
-                smatch m;
-                regex integer("(\\+|-)?[[:digit:]]+");
-                regex_search(str_filename, m, regex("(\\+|-)?[[:digit:]]+"));
-                for(auto v: m){
-                    stringstream geek(v);
-                    geek >> textures[i].index;
-                }
-                regex_search(str_filename, m, regex("shoot/shoot"));            for(auto v: m){textures[i].animation = "shoot_texture";} 
-                regex_search(str_filename, m, regex("flashlight/meleeattack")); for(auto v: m){textures[i].animation = "flashlight_meleeattack";} 
-                regex_search(str_filename, m, regex("flashlight/move"));        for(auto v: m){textures[i].animation = "flashlight_move";} 
-                regex_search(str_filename, m, regex("flashlight/idle"));        for(auto v: m){textures[i].animation = "flashlight_idle";} 
-                regex_search(str_filename, m, regex("feet/walk"));              for(auto v: m){textures[i].animation = "feet_walk";} 
-                regex_search(str_filename, m, regex("feet/strafe_left"));       for(auto v: m){textures[i].animation = "feet_strafe_left";} 
-                regex_search(str_filename, m, regex("feet/strafe_right"));      for(auto v: m){textures[i].animation = "feet_strafe_right";} 
-                regex_search(str_filename, m, regex("feet/run"));               for(auto v: m){textures[i].animation = "feet_run";} 
-                regex_search(str_filename, m, regex("feet/idle"));              for(auto v: m){textures[i].animation = "feet_idle";} 
-                regex_search(str_filename, m, regex("knife/meleeattack"));      for(auto v: m){textures[i].animation = "knife_meleeattack";} 
-                regex_search(str_filename, m, regex("knife/move"));             for(auto v: m){textures[i].animation = "knife_move";} 
-                regex_search(str_filename, m, regex("knife/idle"));             for(auto v: m){textures[i].animation = "knife_idle";} 
-                regex_search(str_filename, m, regex("handgun/meleeattack"));    for(auto v: m){textures[i].animation = "handgun_meleeattack";} 
-                regex_search(str_filename, m, regex("handgun/move"));           for(auto v: m){textures[i].animation = "handgun_move";} 
-                regex_search(str_filename, m, regex("handgun/shoot"));          for(auto v: m){textures[i].animation = "handgun_shoot";} 
-                regex_search(str_filename, m, regex("handgun/reload"));         for(auto v: m){textures[i].animation = "handgun_reload";} 
-                regex_search(str_filename, m, regex("handgun/idle"));           for(auto v: m){textures[i].animation = "handgun_idle";} 
-                regex_search(str_filename, m, regex("rifle/meleeattack"));      for(auto v: m){textures[i].animation = "rifle_meleeattack";} 
-                regex_search(str_filename, m, regex("rifle/move"));             for(auto v: m){textures[i].animation = "rifle_move";} 
-                regex_search(str_filename, m, regex("rifle/shoot"));            for(auto v: m){textures[i].animation = "rifle_shoot";} 
-                regex_search(str_filename, m, regex("rifle/reload"));           for(auto v: m){textures[i].animation = "rifle_reload";} 
-                regex_search(str_filename, m, regex("rifle/idle"));             for(auto v: m){textures[i].animation = "rifle_idle";} 
-                regex_search(str_filename, m, regex("shotgun/meleeattack"));    for(auto v: m){textures[i].animation = "shotgun_meleeattack";} 
-                regex_search(str_filename, m, regex("shotgun/move"));           for(auto v: m){textures[i].animation = "shotgun_move";} 
-                regex_search(str_filename, m, regex("shotgun/shoot"));          for(auto v: m){textures[i].animation = "shotgun_shoot";} 
-                regex_search(str_filename, m, regex("shotgun/reload"));         for(auto v: m){textures[i].animation = "shotgun_reload";} 
-                regex_search(str_filename, m, regex("shotgun/idle"));           for(auto v: m){textures[i].animation = "shotgun_idle";} 
-                cout << "Loading...  " << str_filename << endl;
-                i++;
-            }
-        }
-    }
-}
-
-void myIdle(){
-    game_time++;
-    player.update(game_time);
+void updateGame(){
+    game_clock++;
     player.caminha(keyStates);
-    if (game_time == (421+43)*frame_time*10)
-        game_time = 0;
+    if (time_flag != game_clock / frame_time){
+        time_flag = game_clock / frame_time;
+        cout << game_clock / frame_time << endl;
+        player.update(game_clock);
+        // zombie.update(game_clock, player);
+    }
+    if (game_clock == (421+43)*10000){
+        game_clock = 0;
+    }
 }
 
 void mouseClicks(int button, int state, int x, int y){
@@ -134,18 +83,17 @@ void mouseClicks(int button, int state, int x, int y){
             player.shoot = true;
             player.rand_offset = (rand() % player.arma.accuracy) - 2.5;
             player.shoot_left = 3 * frame_time;
-            game_time = 0;
+            game_clock = 0;
         }else{
             player.shoot = false;
         }
     }
     if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN){
         player.attack = true;
-        game_time = 0;
+        game_clock = 0;
     }else{
         player.attack = false;
     }
-    
 }
 
 void keyDown(unsigned char tecla, int x, int y){
@@ -204,7 +152,7 @@ void keyUp(unsigned char tecla, int x, int y){
 
 }
 
-void cursormouse(int x, int y){
+void mouseMove(int x, int y){
     player.run = (glutGetModifiers() & GLUT_ACTIVE_SHIFT);
     mira.update(x,y);
     player.rotate(x,y);
@@ -213,23 +161,80 @@ void cursormouse(int x, int y){
     texto.set_texto(temp);
 }
 
-void atirando(int x, int y){
+void mouseDrag(int x, int y){
     mira.update(x,y);
     player.rotate(x, y);
 }
 
-void myReshape(GLsizei w, GLsizei h){ 
-    glViewport(0, 0, screen_w, screen_h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(60.0, 1.0 * (GLfloat) w / (GLfloat) h, 1.0, 60.0);
-    glMatrixMode(GL_MODELVIEW);
+void loadTextures(){
+    glClearColor(0,0,0,1);
+    std::string path = "./_player/";
+    string path_string_array[(421+43)];
+    string str_filename; 
+    int i = 0;
+    for (auto & p : fs::directory_iterator(path)){
+        for (auto & pp : fs::directory_iterator(p)){
+            for (auto & ppp : fs::directory_iterator(pp)){
+                str_filename = ppp.path().string().c_str();
+                path_string_array[i] = ppp.path().string().c_str();
+                char *filename = new char[path_string_array[i].size()+1];
+                strncpy(filename, path_string_array[i].c_str(), path_string_array[i].size());
+                textures[i].data = png_texture_load(filename, NULL, NULL);
+
+                smatch m;
+                regex integer("(\\+|-)?[[:digit:]]+");
+                regex_search(str_filename, m, regex("(\\+|-)?[[:digit:]]+"));
+                for(auto v: m){
+                    stringstream geek(v);
+                    geek >> textures[i].index;
+                }
+                regex_search(str_filename, m, regex("shoot/shoot"));            for(auto v: m){textures[i].animation = "shoot_texture";} 
+                regex_search(str_filename, m, regex("flashlight/meleeattack")); for(auto v: m){textures[i].animation = "flashlight_meleeattack";} 
+                regex_search(str_filename, m, regex("flashlight/move"));        for(auto v: m){textures[i].animation = "flashlight_move";} 
+                regex_search(str_filename, m, regex("flashlight/idle"));        for(auto v: m){textures[i].animation = "flashlight_idle";} 
+                regex_search(str_filename, m, regex("feet/walk"));              for(auto v: m){textures[i].animation = "feet_walk";} 
+                regex_search(str_filename, m, regex("feet/strafe_left"));       for(auto v: m){textures[i].animation = "feet_strafe_left";} 
+                regex_search(str_filename, m, regex("feet/strafe_right"));      for(auto v: m){textures[i].animation = "feet_strafe_right";} 
+                regex_search(str_filename, m, regex("feet/run"));               for(auto v: m){textures[i].animation = "feet_run";} 
+                regex_search(str_filename, m, regex("feet/idle"));              for(auto v: m){textures[i].animation = "feet_idle";} 
+                regex_search(str_filename, m, regex("knife/meleeattack"));      for(auto v: m){textures[i].animation = "knife_meleeattack";} 
+                regex_search(str_filename, m, regex("knife/move"));             for(auto v: m){textures[i].animation = "knife_move";} 
+                regex_search(str_filename, m, regex("knife/idle"));             for(auto v: m){textures[i].animation = "knife_idle";} 
+                regex_search(str_filename, m, regex("handgun/meleeattack"));    for(auto v: m){textures[i].animation = "handgun_meleeattack";} 
+                regex_search(str_filename, m, regex("handgun/move"));           for(auto v: m){textures[i].animation = "handgun_move";} 
+                regex_search(str_filename, m, regex("handgun/shoot"));          for(auto v: m){textures[i].animation = "handgun_shoot";} 
+                regex_search(str_filename, m, regex("handgun/reload"));         for(auto v: m){textures[i].animation = "handgun_reload";} 
+                regex_search(str_filename, m, regex("handgun/idle"));           for(auto v: m){textures[i].animation = "handgun_idle";} 
+                regex_search(str_filename, m, regex("rifle/meleeattack"));      for(auto v: m){textures[i].animation = "rifle_meleeattack";} 
+                regex_search(str_filename, m, regex("rifle/move"));             for(auto v: m){textures[i].animation = "rifle_move";} 
+                regex_search(str_filename, m, regex("rifle/shoot"));            for(auto v: m){textures[i].animation = "rifle_shoot";} 
+                regex_search(str_filename, m, regex("rifle/reload"));           for(auto v: m){textures[i].animation = "rifle_reload";} 
+                regex_search(str_filename, m, regex("rifle/idle"));             for(auto v: m){textures[i].animation = "rifle_idle";} 
+                regex_search(str_filename, m, regex("shotgun/meleeattack"));    for(auto v: m){textures[i].animation = "shotgun_meleeattack";} 
+                regex_search(str_filename, m, regex("shotgun/move"));           for(auto v: m){textures[i].animation = "shotgun_move";} 
+                regex_search(str_filename, m, regex("shotgun/shoot"));          for(auto v: m){textures[i].animation = "shotgun_shoot";} 
+                regex_search(str_filename, m, regex("shotgun/reload"));         for(auto v: m){textures[i].animation = "shotgun_reload";} 
+                regex_search(str_filename, m, regex("shotgun/idle"));           for(auto v: m){textures[i].animation = "shotgun_idle";} 
+                cout << "Loading...  " << str_filename << endl;
+                i++;
+            }
+        }
+    }
+}
+
+void reshapeGame(GLsizei w, GLsizei h){ 
+    glViewport(0, 0, screen_w, screen_h);       // Normaliza tamanho da janela com o display
+    glMatrixMode(GL_PROJECTION);                // ... ?
+    glLoadIdentity();                           // Reseta matiz
+    GLfloat aspect = (GLfloat) w / (GLfloat) h; // Proporção da tela
+    gluPerspective(60.0, aspect, 1.0, 60.0);    // Austa proporção e perspectiva da camera
+    glMatrixMode(GL_MODELVIEW);                 // ... ?
 }
 
 void initCallbacks(){
-    glutReshapeFunc(myReshape);
-    glutDisplayFunc(myDisplay);
-    glutIdleFunc(myIdle);                      // Seta função de updateção
+    glutReshapeFunc(reshapeGame);               // Seta reshapeGame como calback da ReshapeFunc
+    glutDisplayFunc(renderGame);                // Seta renderGame como calback da DisplayFunc
+    glutIdleFunc(updateGame);                   // Seta updateGame como calback da IdleFunc
 }
 
 void initKeyboard(){
@@ -239,17 +244,17 @@ void initKeyboard(){
 }
 
 void initMouse(){
-    glutPassiveMotionFunc(cursormouse);         // Callback do cursor movendo 
-    glutMouseFunc(mouseClicks);                 // Callback do click
-    glutMotionFunc(atirando);                   // Callback do drag
-    glutSetCursor(GLUT_CURSOR_NONE);            // Esconde cursor
+    glutPassiveMotionFunc(mouseMove);            // Callback do cursor movendo 
+    glutMouseFunc(mouseClicks);                  // Callback do click
+    glutMotionFunc(mouseDrag);                   // Callback do drag
+    glutSetCursor(GLUT_CURSOR_NONE);             // Esconde cursor
 }
 
 int main(int argc, char** argv) {
     srand(time(NULL));
     glutInit(&argc, argv);                       // Inicializa GLUT
     glutInitWindowSize(screen_w, screen_h);      // Inicializa tamanho da janela
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH);  // Inicializa o display mode
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH); // Inicializa o display mode
     glutCreateWindow("PNG texture");             // Inicializa a janaela
     initCallbacks();                             // Inicializa callbacks
     loadTextures();                              // Inicializa textures
